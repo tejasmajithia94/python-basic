cf557d28a8e3577a7ff4c4fcd77536edd94fa04d
tejasmajithia94 <tejasmajithia94@gmail.com>
2026-09-13 20:52:23 +0530

    Add files via upload

--- Python_Basic.py
+++ Python_Basic.py
@@ -1,14 +1,36 @@
-print('Hello World')
-shot = "SH010"
-print(type(shot))
-print(shot)
-shot = "SH010"
-frames = 120
-fps = 24.0
-approved = False
-print(type(shot))
-print(type(frames))
-print(type(fps))
-print(type(approved))
-print(frames)
-print(fps)
+# print('Hello World')
+# shot = "SH010"
+# print(type(shot))
+# print(shot)
+# shot = "SH010"
+# frames = 120
+# fps = 24.0
+# approved = False
+# print(type(shot))
+# print(type(frames))
+# print(type(fps))
+# print(type(approved))
+# print(frames)
+# print(fps)
+# shot = "SH0010_comp_V003"
+# print(shot[0])
+# print(shot[-1])
+# print(shot[0:6])
+# print(shot[-3:])
+# shot = "SH0010"
+# artist = "Tejas"
+# version = 3
+# print(f"shot:{shot}, Artist: {artist}, Version: v{version:03d}")
+# show = "CIP"
+# shot = "SH0010"
+# task = "comp"
+# version = 4
+# frames = 145
+# fps = 24
+# duration = round(frames / fps, 2)
+# print(duration)
+# frames = 180
+# fps = 25
+# duration = round(frames / fps, 3)
+# print(duration)
+filename ="SH0230_lightning_v012.exr"
