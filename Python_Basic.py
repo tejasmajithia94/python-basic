4c891f26536ab28becdc15cc4963212e51ce25b2
tejasmajithia94 <tejasmajithia94@gmail.com>
2026-09-13 13:43:16 +0530

    Add files via upload

---
+++ Python_Basic.py
@@ -0,0 +1,14 @@
+print('Hello World')
+shot = "SH010"
+print(type(shot))
+print(shot)
+shot = "SH010"
+frames = 120
+fps = 24.0
+approved = False
+print(type(shot))
+print(type(frames))
+print(type(fps))
+print(type(approved))
+print(frames)
+print(fps)
